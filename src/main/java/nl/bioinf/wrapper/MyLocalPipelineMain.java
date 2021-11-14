12f1266caf4a62563137448cca07206ca956129c
Rose Hazenberg <rosehazenberg@gmail.com>
2021-11-14 21:37:28 +0100

    Delete MyLocalPipelineMain.java

--- src/main/java/nl/bioinf/wrapper/MyLocalPipelineMain.java
+++
@@ -1,8 +0,0 @@
-package nl.bioinf.wrapper;
-
-public class MyLocalPipelineMain {
-    public static void main(String[] args) {
-        AlgorithmEngine engine = new AlgorithmEngine();
-        engine.start(new PipelineOptionsProvider());
-    }
-}
